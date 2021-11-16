3795a71b574cf1b4e7e96937d74f819fa9e5a952
Michelle Wehr <michelle_wehr@icloud.com>
2021-11-15 19:06:22 -0500

    implemented indexDb for storing if user goes offline and automatically posts to database when internet returns

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt');
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
